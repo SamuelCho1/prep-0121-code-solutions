5c893091b466ba3aa408f1f1d52354c7c54156d1
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-13 11:14:59 -0800

    Completed addTwoNumbers, convertHoursToMinutes and personalizeGreeting functions.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,26 @@
+// addTwoNumbers Exercise
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+var addTwoNumbersResults = addTwoNumbers(4, 4);
+
+console.log('addTwoNumbers Exercise:', addTwoNumbersResults);
+
+// convertHoursToMinutes Exercise
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+
+var convertHoursToMinutesResults = convertHoursToMinutes(2);
+
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResults);
+
+// personalizeGreeting Exercise
+function personalizeGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var personalizeGreetingResults = personalizeGreeting('World');
+
+console.log('personalizeGreeting Exercise:', personalizeGreetingResults);
