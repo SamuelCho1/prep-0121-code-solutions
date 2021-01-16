5ca5041f47683750a8eebcad0e74dd0004d2589f
Samuel Cho <samuel.ms.cho@gmail.com>
2021-01-16 12:41:04 -0800

    Repeating code blocks using JavaScript for loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,18 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (i = 0; i < 50; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+for (i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (i = 100; i > 0; i--) {
+  console.log('Time till explosion:', i + '!');
+}
